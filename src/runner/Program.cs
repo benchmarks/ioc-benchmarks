d1c70b3df971739cc41116e0bda3ea7b3bddf644
Eugene Sadovoi <evgeni@eniks.com>
2023-05-26 11:52:56 -0700

    Initial setup

---
+++ src/runner/Program.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace ioc.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DefaultConfig.Instance
+                .AddJob(Job.Default.AsDefault())
+                .WithOptions(ConfigOptions.DisableOptimizationsValidator);
+
+            BenchmarkSwitcher.FromAssemblies(new[] 
+            {
+                typeof(Program).Assembly,
+            }).Run(args);
+        }
+    }
+}
